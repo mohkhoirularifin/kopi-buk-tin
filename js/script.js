4a97590517c54d8f240d68dc0ec6e071f9eb4c07
Moh. Khoirul Arifin <43515899+mohkhoirularifin@users.noreply.github.com>
2023-09-13 22:34:33 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toogle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger Menu Diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
